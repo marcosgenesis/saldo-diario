3e8a88dbf5f324527d4e34de47bbfa0b30d9f545
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2025-09-03 20:50:22 -0300

    fix: origin

--- src/services/auth-client.ts
+++ src/services/auth-client.ts
@@ -1,4 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_URL, // The base URL of your auth server
+  fetchOptions: {
+    credentials: "include",
+  },
 });
