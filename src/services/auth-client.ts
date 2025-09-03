25c6894418c848179f7116c5d8ff87daf687188c
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2025-09-03 20:13:01 -0300

    fix: login auth

--- src/services/auth-client.ts
+++ src/services/auth-client.ts
@@ -1,4 +1,4 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:4000" // The base URL of your auth server
-})
+  baseURL: import.meta.env.VITE_API_URL, // The base URL of your auth server
+});
